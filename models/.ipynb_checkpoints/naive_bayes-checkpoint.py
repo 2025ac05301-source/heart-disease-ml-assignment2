2991aa20530b394828bba2b06ae7752159de1e9e
2025ac05301-source <2025ac05301@wilp.bits-pilani.ac.in>
2026-08-11 22:30:52 +0530

    removing ipynb checkpoints and pycache folder

--- models/.ipynb_checkpoints/naive_bayes-checkpoint.py
+++
@@ -1,6 +0,0 @@
-from sklearn.naive_bayes import GaussianNB
-
-def train_model(X_train_scaled, y_train):
-    model = GaussianNB()
-    model.fit(X_train_scaled, y_train)
-    return model
--- models/.ipynb_checkpoints/scaler-checkpoint.py
+++
@@ -1,7 +0,0 @@
-from sklearn.preprocessing import StandardScaler
-
-def get_scaler(X_train):
-    """Fits and returns the standard scaler using training features only."""
-    scaler = StandardScaler()
-    scaler.fit(X_train)
-    return scaler
